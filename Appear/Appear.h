1d8541001f25d19a5d4d106a93ff0c621f517e60
maquannene <maquannene@gmail.com>
2016-12-21 22:14:00 +0800

    Initial Commit

---
+++ Appear/Appear.h
@@ -0,0 +1,19 @@
+//
+//  Appear.h
+//  Appear
+//
+//  Created by 马权 on 21/12/2016.
+//  Copyright © 2016 马权. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Appear.
+FOUNDATION_EXPORT double AppearVersionNumber;
+
+//! Project version string for Appear.
+FOUNDATION_EXPORT const unsigned char AppearVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Appear/PublicHeader.h>
+
+
